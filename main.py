1ddf0cd9e1197f3112a0735b1fba965b4c58b631
Diego R <jdrios003@gmail.com>
2026-04-20 20:50:24 -0700

    Adding uv files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routers import courses as courses_router
 from api.routers import health as health_router
+import uvicorn
 
 app = FastAPI(
     title="API Courses Scrapping",
@@ -21,3 +22,5 @@ app.include_router(courses_router.router, prefix="/api/v1")
 app.include_router(health_router.router, prefix="/api/v1")
 
 
+if __name__ == "__main__":
+    uvicorn.run(app=app, host="0.0.0.0", port=8000)
